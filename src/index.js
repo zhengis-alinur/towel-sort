99b16bc75336a0fb5c58c43ea34d74479f298b46
zhengis-alinur <zhengis.alinur.erzhan@gmail.com>
2021-03-09 13:42:35 +0600

    init: find solution for towel-sort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,22 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if(typeof matrix == 'undefined') {
+    return [];
+  } else if(matrix.length == 0) {
+    return [];
+  }
+  result = [];
+  for(i = 0; i < matrix.length; i++) {
+    if(i % 2 != 0) {
+      for(x = matrix[i].length - 1; x >= 0; x--) {
+        result.push(matrix[i][x]);
+      }
+    } else {
+      for(y = 0; y < matrix[i].length; y++) {
+        result.push(matrix[i][y]);
+      }
+    }
+  }
+  return result;
 }
